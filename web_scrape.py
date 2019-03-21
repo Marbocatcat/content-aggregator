8518dea87cf04028478da7f963bc91b51a0f6e7d
Mar Bocatcat <mar.bocatcat@gmail.com>
2019-03-20 20:13:47 -0500

    first commit

---
+++ web_scrape.py
@@ -0,0 +1,28 @@
+from requests_html import HTMLSession
+
+''' Content aggregator using requests_html '''
+
+# Instantiate HTMLSession object
+session = HTMLSession()
+
+# Grab links and topics from https://realpython.com
+
+
+def get_realpython():
+    # Start HTMLSession.
+    realpython = session.get('https://realpython.com')
+    # Link targets that will grab the link and text we need.
+    target = 'body > div.container.main-content > div.row > \
+    div.col-12.col-md-6.col-lg-4.mb-5 > div.card.border-0 > \
+    div.card-body.m-0.p-0.mt-2 > a'
+
+    scraped_object = realpython.html.find(target)
+
+    for index, links in enumerate(scraped_object):
+        print(links.absolute_links, links.text)
+        if index == 5:
+            print('----- Finished ------')
+            break
+
+
+get_realpython()
